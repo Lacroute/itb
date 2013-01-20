14704934bc712a34ac7c99576831d42634ba356f
BastienPenalba <bastien.penalba@gmail.com>
2013-01-20 22:37:01 +0100

    DB commented

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ $f3->config('config/globals.cfg');
 $f3->config('config/routes.cfg');
 
 // On set la DB
-$db=new DB\SQL(
-    'mysql:host=localhost;port=3306;dbname=mysqldb',
-    'admin',
-    'p455w0rD'
-);
+// $db=new DB\SQL(
+//     'mysql:host=localhost;port=3306;dbname=mysqldb',
+//     'admin',
+//     'p455w0rD'
+// );
 
 
 
