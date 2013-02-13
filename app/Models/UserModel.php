74da56ae19fe3bb6a6ae86a2c4c7b60fe73a8f49
BastienPenalba <bastien.penalba@gmail.com>
2013-02-13 15:30:18 +0100

    fix login

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -25,7 +25,7 @@ class UserModel extends Prefab{
 
 		}elseif (isset($data['email']) && isset($data['password'])) {
 			$user=new DB\SQL\Mapper(F3::get('dB'),'user');
-			$user->load(array('email=? AND password=?',$data['email'],md5($data['password']));
+			$user->load(array('email=? AND password=?',$data['email'],md5($data['password'])));
 			echo "<pre>";
 			var_dump($user);
 			echo "</pre>";
