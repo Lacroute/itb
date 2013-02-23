1b1606f4f67d0a2cd7c9b8a57ee391021fcf6f41
Anthony Zaccardi <zaccardi.anthony@gmail.com>
2013-02-23 16:22:20 +0100

    Dash et home avec quelques modifs (header, select medias, …)

--- home + dash/js/main.js
+++ home + dash/js/main.js
@@ -1,1 +1,17 @@
+(function() {
 
+	$('#connect').addClass('connect');
+
+	$('a[href^="#"]').click(function(){  
+	    var the_id = $(this).attr("href");  
+	    $('html, body').animate({  
+	        scrollTop:$(the_id).offset().top
+	    }, 'slow');  
+	    return false;  
+	});
+
+	$('article.info').click(function(){
+		$(this).toggleClass('active');
+	});
+
+})();
