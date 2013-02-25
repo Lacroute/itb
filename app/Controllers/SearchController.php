7988ef725aa4ab1795fcbba892a8e35831285ab2
Procureur <mc.flurry@hotmail.fr>
2013-02-25 23:22:13 +0100

    ajax call on progress

--- app/Controllers/SearchController.php
+++ app/Controllers/SearchController.php
@@ -26,6 +26,31 @@ class SearchController{
 	    echo View::instance()->render('admin/dashboard.html');
 	}
 
+	function searchForAjax(){
+		$api = F3::get('PARAMS.api');
+		header('Cache-Control: no-cache, must-revalidate');
+		header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+		header('Content-type: application/json');
+		switch ($api) {
+			case 'synonyms':
+				$result = F3::get('synonyms');
+				break;
+			case 'dribbble':
+				$result = F3::get('dribbble');
+				break;
+			case 'pinterest':
+				$result = F3::get('pinterest');
+				break;
+			case 'news':
+				$result = F3::get('news');
+				break;
+			default:
+				$result = "nixamere";
+				break;
+		}
+		echo $result;
+	}
+
 	function __destruct(){
 
 	}
